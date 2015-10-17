1a4c14f58188414f6a1f18c04f805cc1624fa384
gitaeks <break@kimsq.com>
2015-10-18 07:50:10 +0900

    rb_layout_mylayout3 개선

--- layouts/mylayout3/default.php
+++ layouts/mylayout3/default.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html lang="ko">
-<head>
-<?php include $g['dir_layout'].'/_includes/_import.head.php'?>
-</head>
-<body id="rb-body">
+	<head>
+		<?php include $g['dir_layout'].'/_includes/_import.head.php' ?>
+	</head>
+	<body>
 
-	<div id="content-main">
-	<?php include __KIMS_CONTENT__ ?>
-	</div>
+		<div class="container">
+			<?php include __KIMS_CONTENT__ ?>
+		</div>
 
-	<?php include $g['dir_layout'].'/_includes/_import.foot.php'?>
-</body>
+		<?php include $g['dir_layout'].'/_includes/_import.foot.php' ?>
+	</body>
 </html>
