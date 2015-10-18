a4bb6c285eefdd361e191297027bf26946ccc4bd
gitaeks <break@kimsq.com>
2015-10-18 09:00:16 +0900

    레이아웃 내장 초기페이지 수정

--- layouts/mylayout2/_pages/main.php
+++ layouts/mylayout2/_pages/main.php
@@ -1,6 +1,4 @@
-<div class="rb-front">
-	<p>
-		이 페이지는 레이아웃에 포함되어 있는 메인화면입니다<br>
-		이 파일의 위치 : <?php echo __FILE__?>
-	</p>
-</div>
+<p class="rb-front">
+	이 페이지는 레이아웃에 포함되어 있는 메인화면입니다<br>
+	이 파일의 위치 : <?php echo __FILE__?>
+</p>
--- layouts/mylayout3/_pages/main.php
+++ layouts/mylayout3/_pages/main.php
@@ -1,4 +1,4 @@
-<div class="rb-front">
+<p class="rb-front">
 	이 페이지는 레이아웃에 포함되어 있는 메인화면입니다<br>
 	이 파일의 위치 : <?php echo __FILE__?>
-</div>
+</p>
